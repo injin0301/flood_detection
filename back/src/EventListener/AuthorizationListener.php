0c8fa594b4188ac90709d8f477e4303787e3d71d
Gio <giovanni.ricotta@ynov.com>
2025-03-02 17:12:37 +0100

    Add Symfony HTTP client dependency and enhance AuthorizationListener to fetch weather data

--- back/src/EventListener/AuthorizationListener.php
+++ back/src/EventListener/AuthorizationListener.php
@@ -2,18 +2,26 @@
 
 namespace App\EventListener;
 
+use App\Entity\Capteur;
 use App\Repository\HexTextProtectRepository;
+use App\Repository\PieceRepository;
 use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AuthorizationListener
 {
     public function __construct(
         private JWTTokenManagerInterface $jwtManager,
         private UtilisateurRepository $uRepository,
+        private PieceRepository $pRepository,
+        private EntityManagerInterface $em,
         private HexTextProtectRepository $hexTextProtectRepository,
+        private HttpClientInterface $client
     ) {
     }
 
@@ -27,6 +35,8 @@ class AuthorizationListener
         if (in_array($request->getPathInfo(), ['/api/login', '/api/register/user', '/api/doc'])) {
             return;
         }
+
+        $this->gerRecupData();
 
         $authorizationHeader = $request->headers->get('Authorization');
         if (!$authorizationHeader) {
@@ -65,6 +75,34 @@ class AuthorizationListener
             $request->attributes->set('jwt_token', $parsedToken);
         } catch (\Exception $e) {
             throw new AccessDeniedHttpException($e->getMessage());
+        }
+    }
+
+    public function gerRecupData(): void
+    {
+        $pieces = $this->pRepository->findAll();
+        if (empty($pieces)) {
+            return;
+        }
+
+        $response = $this->client->request('GET', 'https://api.tutiempo.net/json/?lan=fr&apid=zwDX4azaz4X4Xqs&ll=40.4178,-3.7022');
+        if ($response->getStatusCode() !== 200) {
+            return;
+        }
+
+        $data = $response->toArray();
+        foreach ($pieces as $piece) {
+            foreach ($data as $value) {
+                foreach (['day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7'] as $day) {
+                    $capteur = new Capteur();
+                    $capteur->setHumidite($value[$day]['humidity']);
+                    $capteur->setTemperature($value[$day]['temperature_max']);
+                    $capteur->setNiveauEau($value[$day]['icon']);
+                    $capteur->setInondation($value[$day]['icon'] > 10);
+                    $this->em->persist($capteur);
+                    $piece->addCapteur($capteur);
+                }
+            }
         }
     }
 }
